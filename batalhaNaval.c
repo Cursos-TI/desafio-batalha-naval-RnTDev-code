a7644180c1d9c42be7729b9277d9d18762395f6c
RnTDev-code <rntindev@gmail.com>
2025-08-26 21:05:15 +0000

    Batalha naval lvl mestre!

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,102 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define TAMANHO_TABULEIRO 10
+#define TAMANHO_HABILIDADE 5
+#define AGUA 0
+#define NAVIO 3
+#define AREA_AFETADA 1
+#define AREA_NORMAL 0
+
+void imprimirTabuleiro(int tamanho, int tabuleiro[tamanho][tamanho]);
+void gerarHabilidadeCone(int tamanho, int matriz[tamanho][tamanho]);
+void gerarHabilidadeCruz(int tamanho, int matriz[tamanho][tamanho]);
+void gerarHabilidadeOctaedro(int tamanho, int matriz[tamanho][tamanho]);
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    printf("--- Desafio Batalha Naval: Nivel Mestre --- \n\n");
+    printf("--- Tabuleiro Principal do Jogo --- \n");
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    int tabuleiroPrincipal[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO] = {0};
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    for (int j = 1; j <= 3; j++) {
+        tabuleiroPrincipal[1][j] = NAVIO;
+    }
+    for (int i = 4; i <= 6; i++) {
+        tabuleiroPrincipal[i][8] = NAVIO;
+    }
+    for (int i = 3; i <= 5; i++) {
+        tabuleiroPrincipal[i][i] = NAVIO;
+    }
+    for (int i = 0; i <= 2; i++) {
+        tabuleiroPrincipal[7 + i][2 - i] = NAVIO;
+    }
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    imprimirTabuleiro(TAMANHO_TABULEIRO, tabuleiroPrincipal);
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    int matrizCone[TAMANHO_HABILIDADE][TAMANHO_HABILIDADE];
+    int matrizCruz[TAMANHO_HABILIDADE][TAMANHO_HABILIDADE];
+    int matrizOctaedro[TAMANHO_HABILIDADE][TAMANHO_HABILIDADE];
+
+    printf("\n--- Habilidade Especial: Cone --- \n");
+    gerarHabilidadeCone(TAMANHO_HABILIDADE, matrizCone);
+    imprimirTabuleiro(TAMANHO_HABILIDADE, matrizCone);
+
+    printf("\n--- Habilidade Especial: Cruz --- \n");
+    gerarHabilidadeCruz(TAMANHO_HABILIDADE, matrizCruz);
+    imprimirTabuleiro(TAMANHO_HABILIDADE, matrizCruz);
+
+    printf("\n--- Habilidade Especial: Octaedro --- \n");
+    gerarHabilidadeOctaedro(TAMANHO_HABILIDADE, matrizOctaedro);
+    imprimirTabuleiro(TAMANHO_HABILIDADE, matrizOctaedro);
 
     return 0;
+}
+
+void imprimirTabuleiro(int tamanho, int tabuleiro[tamanho][tamanho]) {
+    for (int i = 0; i < tamanho; i++) {
+        for (int j = 0; j < tamanho; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void gerarHabilidadeCone(int tamanho, int matriz[tamanho][tamanho]) {
+    int centro = tamanho / 2;
+    for (int i = 0; i < tamanho; i++) {
+        for (int j = 0; j < tamanho; j++) {
+            if (i >= centro && j >= centro - (i - centro) && j <= centro + (i - centro)) {
+                matriz[i][j] = AREA_AFETADA;
+            } else {
+                matriz[i][j] = AREA_NORMAL;
+            }
+        }
+    }
+}
+
+void gerarHabilidadeCruz(int tamanho, int matriz[tamanho][tamanho]) {
+    int centro = tamanho / 2;
+    for (int i = 0; i < tamanho; i++) {
+        for (int j = 0; j < tamanho; j++) {
+            if (i == centro || j == centro) {
+                matriz[i][j] = AREA_AFETADA;
+            } else {
+                matriz[i][j] = AREA_NORMAL;
+            }
+        }
+    }
+}
+
+void gerarHabilidadeOctaedro(int tamanho, int matriz[tamanho][tamanho]) {
+    int centro = tamanho / 2;
+    for (int i = 0; i < tamanho; i++) {
+        for (int j = 0; j < tamanho; j++) {
+            int distancia = (i > centro ? i - centro : centro - i) + (j > centro ? j - centro : centro - j);
+            if (distancia <= centro) {
+                matriz[i][j] = AREA_AFETADA;
+            } else {
+                matriz[i][j] = AREA_NORMAL;
+            }
+        }
+    }
 }
